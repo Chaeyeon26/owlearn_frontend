61201e638b67e86b0574144347979b325ec186a8
Chaeyeon26 <chaeyeon@naver.com>
2025-05-14 16:42:11 +0900

    [fix] 프로젝트 경로 재정리

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./component/header";
+import StartPage from "./pages/StartPage";
+import LoginPage from "./pages/LoginPage";
+import Diagnosis from "./pages/DiagnosisPage";
+import CreatePage from "./pages/CreationPage";
+import StudyMain from "./pages/studyMain";
+import StudyProgress from "./pages/studyProgress";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/diagnosis" element={<Diagnosis />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/studyMain" element={<StudyMain />} />
+        <Route path="/tale/study" element={<StudyProgress />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
